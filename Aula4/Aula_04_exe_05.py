46524facc0d9f82f28e00586c22a5bba6651b27d
FlipGoncalves <filipeg@ua.pt>
2022-11-11 15:52:53 +0000

    Aula 4 done

--- Aula4/Aula_04_exe_05.py
+++ Aula4/Aula_04_exe_05.py
@@ -16,21 +16,17 @@ image_art2 = cv2.imread( "images/art2.bmp" , cv2.IMREAD_GRAYSCALE )
 ret, image_art3_bin = cv2.threshold(image_art3, 120, 255, cv2.THRESH_BINARY)
 ret, image_art2_bin = cv2.threshold(image_art2, 120, 255, cv2.THRESH_BINARY)
 
-# inversion of resulting images
-image_art3_inv = cv2.bitwise_not(image_art3_bin)
-image_art2_inv = cv2.bitwise_not(image_art2_bin)
-
 # erosion followed by dilation
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11,11))
-image_art3_er = cv2.erode(image_art3_inv, kernel)
+image_art3_er = cv2.erode(image_art3_bin, kernel)
 image_art3_op = cv2.dilate(image_art3_er, kernel)
 
 kernel = np.ones((9,3), np.uint8)
-image_art2_er1 = cv2.erode(image_art2_inv, kernel)
+image_art2_er1 = cv2.erode(image_art2_bin, kernel)
 image_art2_op1 = cv2.dilate(image_art2_er1, kernel)
 
 kernel = np.ones((3,9), np.uint8)
-image_art2_er2 = cv2.erode(image_art2_inv, kernel)
+image_art2_er2 = cv2.erode(image_art2_bin, kernel)
 image_art2_op2 = cv2.dilate(image_art2_er2, kernel)
 
 cv2.imshow('Orginal Art3', image_art3)
